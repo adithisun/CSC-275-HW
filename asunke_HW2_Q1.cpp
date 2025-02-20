4b8dde9247806cbbd3a93cf4cb914d486ee7e058
adithisun <143733710+adithisun@users.noreply.github.com>
2025-02-20 18:11:59 -0500

    asunke_HW2_Q1.cpp

--- asunke_HW2_Q1.cpp
+++ asunke_HW2_Q1.cpp
@@ -15,15 +15,18 @@
 #include <ctime>
 using namespace std;
 
+//defining the check struct
 struct Check {
     int CheckNum;
     string CheckMemo;
     float CheckAmount;
 
+    //overloaded operator to compare check amount with a float value
     bool operator>(float amount) const {
         return CheckAmount > amount;
     }
 
+    //overloaded operator
     friend ostream& operator<<(ostream& os, const Check& check) {
         os << "Check Num: " << check.CheckNum << " | Amount: $" << check.CheckAmount << " | Memo: " << check.CheckMemo;
         return os;
@@ -31,6 +34,7 @@ struct Check {
 };
 
 class CheckBook {
+    //define the checkbook class with required data members
 private:
     float balance;
     Check* chkPtr;
@@ -38,6 +42,7 @@ private:
     int numOfChecks;
     int checkBookSize;
 
+    //resizing of the check array when half of the checkbook is used
     void expandCheckBook() {
         checkBookSize *= 2;
         Check* newChkPtr = new Check[checkBookSize];
@@ -50,18 +55,22 @@ private:
     }
 
 public:
+    //default constructor
     CheckBook() : balance(0), numOfChecks(0), checkBookSize(4) {
         chkPtr = new Check[checkBookSize];
     }
 
+    //parameterized constructor
     CheckBook(float initialBalance) : balance(initialBalance), numOfChecks(0), checkBookSize(2) {
         chkPtr = new Check[checkBookSize];
     }
 
+    //destructor
     ~CheckBook() {
         delete[] chkPtr;
     }
 
+    //copy constructor
     CheckBook(const CheckBook& other) : balance(other.balance), lastDeposit(other.lastDeposit), numOfChecks(other.numOfChecks), checkBookSize(other.checkBookSize) {
         chkPtr = new Check[checkBookSize];
         for (int i = 0; i < numOfChecks; ++i) {
@@ -69,6 +78,7 @@ public:
         }
     }
 
+    //overloaded operator
     CheckBook& operator=(const CheckBook& other) {
         if (this != &other) {
             delete[] chkPtr;
@@ -84,19 +94,23 @@ public:
         return *this;
     }
 
+    //get functions
     float getBalance() const { return balance; }
     int getNumOfChecks() const { return numOfChecks; }
     int getCheckBookSize() const { return checkBookSize; }
     float getLastDeposit() const { return lastDeposit; }
 
+    //set functions
     void setBalance(float newBalance) { balance = newBalance; }
     void setLastDeposit(float deposit) { lastDeposit = deposit; }
 
+    //deposit function to add amount to balance, as required
     void deposit(float amount) {
         balance += amount;
         lastDeposit = amount;
     }
 
+    //implements writecheck function to process a check, update balance, and select a memo randomly
     bool writeCheck(Check c_amount) {
         if (!(c_amount > balance)) {
             string memos[] = {"party", "girls trip", "pasta", "dental office", "hair salon", "banquet", "electronics", "appliances"};
@@ -114,6 +128,7 @@ public:
         return false;
     }
 
+    //display all checks in order
     void displayChecks() const {
         for (int i = numOfChecks - 1; i >= 0; --i) {
             cout << chkPtr[i] << endl;
@@ -121,12 +136,13 @@ public:
     }
 };
 
+//test the checkbook class by writing checks until the balance is exhausted and doubling checkbook size when needed
 void checkTest(CheckBook& cb, float balance) {
     srand(time(0));
     cb.deposit(balance);
 
     while (cb.getBalance() > 0) {
-        float checkAmount = (rand() % 50) + 1; 
+        float checkAmount = (rand() % 50) + 1; //random small check amount
         Check newCheck = {0, "", checkAmount};
         if (!cb.writeCheck(newCheck)) break;
     }
@@ -134,8 +150,9 @@ void checkTest(CheckBook& cb, float balance) {
     cb.displayChecks();
 }
 
+//main function initializes a checkbook object and calls the checktest function to demonstrate functionality
 int main() {
-    CheckBook cb(500); 
+    CheckBook cb(500); //initialize with high balance
     checkTest(cb, 500);
     return 0;
 }
