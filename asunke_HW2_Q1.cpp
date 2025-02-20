24f4f20dd5f1ceafcd6a9783f6d1438ab20d76a8
adithisun <143733710+adithisun@users.noreply.github.com>
2025-02-20 13:55:33 -0500

    Add files via upload

---
+++ asunke_HW2_Q1.cpp
@@ -0,0 +1,12 @@
+/*************************************************************************
+** Author : Adithi Sunke
+** Program : hw2 q1
+** Date Created : Feb 20th, 2025
+** Date Last Modified : Feb 20th, 2025
+** Usage : No command line arguments
+**
+** Problem:
+ - Implement a Checkbook system where users can write checks, deposit money,
+   and track their balance. The system dynamically expands the checkbook size
+   as needed.
+*************************************************************************/
