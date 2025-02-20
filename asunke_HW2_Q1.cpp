cfd92b563d0e41fc8b068bcc5ebcc9ad9c0e14db
adithisun <143733710+adithisun@users.noreply.github.com>
2025-02-20 14:17:22 -0500

    Add files via upload

---
+++ asunke_HW2_Q1.cpp
@@ -0,0 +1,16 @@
+/*************************************************************************
+** Author : Your Name
+** Program : CheckBook Management
+** Date Created : February 20, 2025
+** Date Last Modified : February 20, 2025
+** Usage : No command line arguments
+**
+** Problem:
+ - Create a Check struct and a CheckBook class to manage check transactions.
+*************************************************************************/
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
