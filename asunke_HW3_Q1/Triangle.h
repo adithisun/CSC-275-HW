b550d61d6eec585d7af4db645e36b5b1c0b9ef65
adithisun <143733710+adithisun@users.noreply.github.com>
2025-03-24 16:51:18 -0400

    Triangle.h

--- asunke_HW3_Q1/Triangle.h
+++ asunke_HW3_Q1/Triangle.h
@@ -5,10 +5,15 @@
 #ifndef ASUNKE_HW3_Q1_TRIANGLE_H
 #define ASUNKE_HW3_Q1_TRIANGLE_H
 
+#include "Polygon.h"
+#include <iostream>
 
-class Triangle {
-
+class Triangle : public Polygon {
+public:
+    Triangle();
+    Triangle(int w, int h);
+    int getArea() override;
+    void draw() override;
 };
-
 
 #endif //ASUNKE_HW3_Q1_TRIANGLE_H
