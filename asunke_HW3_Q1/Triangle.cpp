4fd47403070ac6658acc187d88a42054a432b757
adithisun <143733710+adithisun@users.noreply.github.com>
2025-03-24 16:50:53 -0400

    Triangle.cpp

--- asunke_HW3_Q1/Triangle.cpp
+++ asunke_HW3_Q1/Triangle.cpp
@@ -3,3 +3,33 @@
 //
 
 #include "Triangle.h"
+#include <iostream>
+
+using namespace std;
+
+Triangle::Triangle() : Polygon() {}
+
+Triangle::Triangle(int w, int h) : Polygon(w, h) {}
+
+int Triangle::getArea() {
+    return (width * height) / 2; //triangle's area
+}
+
+//symmetric triangle/centered stars
+void Triangle::draw() {
+    int base_width = (2 * height) - 1;
+    for (int i = 1; i <= height; ++i) {
+        int stars = (2 * i) - 1;
+        int spaces = (base_width - stars) / 2;
+
+        //leading spaces
+        for (int s = 0; s < spaces; ++s)
+            cout << " ";
+
+        //stars
+        for (int j = 0; j < stars; ++j)
+            cout << "*";
+
+        cout << endl;
+    }
+}
