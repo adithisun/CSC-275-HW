5413cbade534f03809793a90174f5f66a9e6e155
adithisun <143733710+adithisun@users.noreply.github.com>
2025-03-24 16:49:53 -0400

    Rectangle.cpp

--- asunke_HW3_Q1/Rectangle.cpp
+++ asunke_HW3_Q1/Rectangle.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "Rectangle.h"
+
+Rectangle::Rectangle() : Polygon() {}
+Rectangle::Rectangle(int w, int h) : Polygon(w, h) {}
+
+int Rectangle::getArea() {
+    return width * height; //rectangle area
+}
+
+void Rectangle::draw() {
+    for (int i = 0; i < width; ++i) {
+        for (int j = 0; j < height; ++j)
+            std::cout << "*";
+        std::cout << std::endl;
+    }
+}
