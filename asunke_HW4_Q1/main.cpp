f31d6859a4185d238dfa5a088822ed95debea213
adithisun <143733710+adithisun@users.noreply.github.com>
2025-04-09 23:33:19 -0400

    main.cpp

--- asunke_HW4_Q1/main.cpp
+++ asunke_HW4_Q1/main.cpp
@@ -15,13 +15,53 @@
 
 using namespace std;
 
+//struct for profile
 struct Profile {
     int userId;
     string username;
 
+    //for printing
     friend ostream& operator<<(ostream& os, const Profile& p) {
         os << "[" << p.userId << "] " << p.username;
         return os;
     }
 };
 
+int main() {
+    //twitter string account
+    Twitter<string> TS("TechGuru", false);
+    TS.addFollower("Alice");
+    TS.addFollowed("Bob");
+    TS.RT();
+    TS.displayFollowers();
+    TS.displayFollowed();
+    cout << "Retweets: " << TS.getRetweetCount() << endl << endl;
+
+    //twitter profile account
+    Profile p1 = {1, "cooldev"};
+    Profile p2 = {2, "coder123"};
+
+    Twitter<Profile> TP(p1, false);
+    TP.addFollower(p2);
+    TP.displayFollowers();
+    TP.RT();
+    cout << "Retweets: " << TP.getRetweetCount() << endl << endl;
+
+    //instagram string account
+    Instagram<string> IS("PhotoQueen", false);
+    IS.addFollower("ShutterBug");
+    IS.Like();
+    IS.displayFollowers();
+    cout << "Likes: " << IS.getLikeCount() << endl << endl;
+
+    // instagram profile account
+    Profile p3 = {3, "snapguy"};
+    Instagram<Profile> IP(p3, true); //private account
+    Profile p4 = {4, "lenslady"};
+    IP.addFollower(p4);
+    IP.displayFollowers(); //will show protected message
+    IP.Like();
+    cout << "Likes: " << IP.getLikeCount() << endl;
+
+    return 0;
+}
