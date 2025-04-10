0fbc427547f317765fa4d89e7d80958ddcfd854a
adithisun <143733710+adithisun@users.noreply.github.com>
2025-04-09 23:32:55 -0400

    Update Instagram.h

--- asunke_HW4_Q1/Instagram.h
+++ asunke_HW4_Q1/Instagram.h
@@ -7,6 +7,22 @@
 
 #include "SocialMediaAccount.h"
 
+template <typename T>
+class Instagram : public SocialMediaAccount<T> {
+private:
+    int likeCount;
 
+public:
+    Instagram() : SocialMediaAccount<T>(), likeCount(0) {}
+
+    Instagram(T handler, bool isPrivate = true)
+            : SocialMediaAccount<T>(handler, isPrivate), likeCount(0) {}
+
+    void Like() { likeCount++; }
+
+    void setLikeCount(int count) { likeCount = count; }
+
+    int getLikeCount() const { return likeCount; }
+};
 
 #endif //ASUNKE_HW4_Q1_INSTAGRAM_H
